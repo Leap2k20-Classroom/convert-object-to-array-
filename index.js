45ea7a16f25873bcc74a7193818b11dcc0a16467
lhagvadarjaa <lhagva98@gmail.com>
2021-01-25 11:03:58 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const object = {
+    '1': 'mnz',
+    '4': 'end',
+    '10': 'usr',
+    '7': 'rug',
+    '13': 'cng'
+}
+
+function convert() {
+    var arr = [];
+    return arr; 
+    //Буцаах arr хувьсагч ийм байх ёстой гэхдээ шууд return хийж болохгүй шүү
+    //['-','mn','-','-','en','-','-','ru','-','-','us','-','-','cn']
+}
+
+console.log(convert(object).join(''))
